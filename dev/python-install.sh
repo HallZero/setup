74308b47bec9912ac07ccd5e64c13c6985dcfda2
HallZero <filipikikuchi@gmail.com>
2024-03-15 11:42:06 -0300

    fix: :clown_face: Add mocked python version

--- dev/python-install.sh
+++ dev/python-install.sh
@@ -8,15 +8,16 @@ fi
 
 # Download the latest Python version
 printf "Downloading the latest Python version...\n"
-LATEST_PYTHON_VERSION="$(curl --silent https://www.python.org/downloads/ | grep -oP 'Python\s\d+\.\d+\.\d+' | head -n 1 | grep -oP '\d+\.\d+\.\d+')"
+LATEST_PYTHON_VERSION="3.11.8"
 
-printf "Downloading Python version %s...\n" "$LATEST_PYTHON_VERSION";
+
+printf "Downloading Python version %s...\n" ${LATEST_PYTHON_VERSION};
 
 curl -OJL --progress-bar https://www.python.org/ftp/python/${LATEST_PYTHON_VERSION}/Python-${LATEST_PYTHON_VERSION}.tgz
 
 printf "Extracting file...\n"
 
-tar -C /usr/local -xzf Python-${LATEST_PYTHON_VERSION}.tgz
+sudo tar -C /usr/local -xzf Python-${LATEST_PYTHON_VERSION}.tgz
 
 rm Python-${LATEST_PYTHON_VERSION}.tgz
 
@@ -29,5 +30,5 @@ alias python=python3
 # Installing pip
 printf "Installing pip...\n"
 
-curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py
 
